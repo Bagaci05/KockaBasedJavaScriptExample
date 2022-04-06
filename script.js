e1057ba41c1bb5932d810c275c67fb501b1b4202
Bagaci <blaga.mate05@gmail.com>
2022-04-06 21:50:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//** KOCKA BASED **\\
+ 
+let penz = 1200
+
+while(penz > 0){
+    let tipus = prompt("MIt kersz") 
+    if(tipus == "koczka"){
+        if(penz >= 100){
+            penz = penz - 100
+            alert("Koczka")
+        }else{
+            alert("Nincs elegendő pénzed")
+        }
+    }
+    else if(tipus == "koczka"){
+        if(penz >= 100){
+            penz = penz - 100
+            alert("Koczka")
+        }else{
+            alert("Nincs elegendő pénzed")
+        }
+    }
+    else if(tipus == "koczka"){
+        if(penz >= 100){
+            penz = penz - 100
+            alert("Koczka")
+        }else{
+            alert("Nincs elegendő pénzed")
+        }
+    }
+    else{
+        alert("Ilyen nincs")
+    }
+}
